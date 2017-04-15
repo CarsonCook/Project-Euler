b1ac51aba9162c1cf10609e82cff30e4b3f43645
Carson Cook <cook.carson38@gmail.com>
2017-04-15 15:33:06 -0400

    finished problem 16

--- problem-16-2sum/main.cpp
+++ problem-16-2sum/main.cpp
@@ -1,51 +1,36 @@
 #include <iostream>
+#include <string>
 #include <sstream>
+#include "BigIntegerLibrary.hh"
+
+#define POWER 1000
 
 using namespace std;
 
-typedef unsigned long long ulll;
-
-string binConversion(unsigned int num);
-ulll modExp(int base, string bin);
+int charToInt(char c);
 
 int main()
 {
-    cout << modExp(2,binConversion(1000));
+    BigInteger num=2;
+    for (int i=0; i<POWER-1; i++)
+    {
+        num*=2;
+    }
+    string digits=bigIntegerToString(num);
+    cout << digits << endl;
+    int sum=0;
+    for (int i=0; i<digits.length(); i++)
+    {
+        sum+=charToInt(digits[i]);
+    }
+    cout << sum << endl;
 }
 
-string binConversion(unsigned int num)
+int charToInt(char c)
 {
     stringstream ss;
-    string binReverse;
-    int rem;
-    do
-    {
-      rem = num%2;
-      num /=2;
-      ss << rem;
-      char one;
-      ss >> one;
-      binReverse +=one;
-    } while (num>0);
-    string out;
-    for (int i = binReverse.length(); i>=0; i--)
-    {
-        out+=binReverse[i];
-    }
-    return out;
-}
-
-ulll modExp(int base, string bin)
-{
-    int x=1;
-    ulll power=base;
-    for (int i=0; i<bin.length(); i++)
-    {
-        if (bin[bin.length()-1-i] == '1')
-        {
-            x=(x*power);
-        }
-        power=(power*power);
-    }
-    return x;
+    ss << c;
+    int i;
+    ss >> i;
+    return i;
 }
