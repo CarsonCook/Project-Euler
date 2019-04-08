3bd9bf1a6e017907d3851dd5de8805f60c2b32f7
Carson Cook <cook.carson38@gmail.com>
2019-04-07 23:58:11 -0400

    init prob 23

---
+++ Problem-23-Non-abundant-Sums/cmake-build-debug/CMakeFiles/Problem_23_Non_abundant_Sums.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Problem_23_Non_abundant_Sums.dir/main.cpp.obj"
+  "Problem_23_Non_abundant_Sums.pdb"
+  "Problem_23_Non_abundant_Sums.exe"
+  "Problem_23_Non_abundant_Sums.exe.manifest"
+  "libProblem_23_Non_abundant_Sums.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Problem_23_Non_abundant_Sums.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
