0f14f528699528b5a3c4dd0576e6e06b83d8dc5d
Carson Cook <cook.carson38@gmail.com>
2019-04-09 12:51:12 -0400

    init prob 25

---
+++ Problem-25-1000-Digit-Fib-Num/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_97d37.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_97d37.dir/testCXXCompiler.cxx.obj"
+  "cmTC_97d37.pdb"
+  "cmTC_97d37.exe"
+  "cmTC_97d37.exe.manifest"
+  "libcmTC_97d37.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_97d37.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Problem-25-1000-Digit-Fib-Num/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_97d37.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmTC_97d37.
+# This may be replaced when dependencies are built.
